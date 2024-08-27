8184e6b5a01e47da9ba8ff98849f8a822f3ee3a8
Ayala <ayalafreire7@gmail.com>
2024-08-27 15:34:14 -0300

    Comentários sobre a classe

--- No.java
+++ No.java
@@ -2,15 +2,15 @@
 public class No<T> {
 
     private T elemento; //elemento vai guardar um valor de qualquer tipo
-    private No<T> proximo;
+    private No<T> proximo; //ponteiro
     
-    // Construtores
+    // Construtor inicializa o nó e define o próximo ponteiro como null
     public No(T elemento) {
         this.elemento = elemento;
         this.proximo = null;
     }
     
-    
+    // Construtor inicializa o nó com um elemento e o ponteiro para o próximo nó
     public No(T elemento, No<T> proximo) {
         this.elemento = elemento;
         this.proximo = proximo;
